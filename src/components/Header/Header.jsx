89daae01b9c14d7f0e94439a82a4d50c3bcb7eba
Akash Chaubey <akashakki9520@gmail.com>
2023-08-30 09:29:55 +0530

    setup react form hook

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+// style
+import "./_header.scss";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">Header</header>
+    </>
+  );
+};
+
+export default Header;
